137552a9dfc353d9c2e79d9a79e53978d1270aa9
hala ibrahim <halaibrahim867@gmail.com>
2024-09-27 19:35:12 +0300

    add routes, model, factory, migrations and controller of product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products=Product::all();
+
+        return response()->json([
+            'status'=>true,
+            'products'=>$products,
+        ]);
+    }
+
+    public function show($id){
+        $product=Product::findOrFail($id);
+
+        return response()->json([
+            'status'=>true,
+            'products'=>$product,
+        ]);
+    }
+}
